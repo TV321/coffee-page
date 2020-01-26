7d07ec02d26c84b16091627b0b27fbc5a6874656
TV321 <tvlahovic7@gmail.com>
2020-01-26 15:45:49 +0100

    navbar style

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,7 +7,7 @@ import Button from 'react-bootstrap/Button';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Container from 'react-bootstrap/Container';
 import '../styles/Navigation.sass';
-import logo from './logo.png'
+import logo from '../images/logo.png'
 
 
 
@@ -16,42 +16,32 @@ const Navigation = () =>{
             <div>
             <Navbar className="nav-style" expand="lg">
                 <Container>
-                <Navbar.Brand href="#home" style={{ display: 'flex'}}>
-                    <img
-                        alt=""
-                        src={ logo }
-                        width="auto"
-                        height="100"
-                        className="d-inline-block align-top"
-                    />{' '}
-                    <p style={{ display: 'inline-block', alignSelf: 'center', margin: '0'}}>Starduck Coffee</p>
+                <Navbar.Brand href="#home">
+                    <div id="nav-logo">
+                        <img
+                            alt=""
+                            src={ logo }
+                            width="auto"
+                            height="80"
+                            className="d-inline-block align-top"
+                        />{' '}
+                        <p id="nav-logo-text">Starduck Coffee</p>
+                    </div>
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                        </NavDropdown>
+
                     </Nav>
                     <Nav>
-                        <Nav.Link href="#deets">
-                            More deets
-                        </Nav.Link>
-                        <Nav.Link href="#memes">
-                            Dank memes
-                        </Nav.Link>
+                        <Nav.Link href="#memes" className="nav-links">Home</Nav.Link>
+                        <Nav.Link href="#memes" className="nav-links">About</Nav.Link>
+                        <Nav.Link href="#memes" className="nav-links">Contact</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
                 </Container>
             </Navbar>
             </div>
-
     )
 }
 
