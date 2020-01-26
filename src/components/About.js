714dde72477eb627c52b673c76c2f4c1f1be0354
TV321 <tvlahovic7@gmail.com>
2020-01-26 22:35:10 +0100

    about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,71 @@
 import React from 'react';
 import '../styles/About.sass';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Card from 'react-bootstrap/Card'
+
 
 const About = () => {
     return(
-            <section id="about">
-                <h3>About section.</h3>
-            </section>
+            <Container id="about">
+              <Row>
+                <Col>
+                    <h2>Etiam efficitur euismod</h2>
+                    <h3>Praesent hendrerit lorem in augue</h3>
+
+                </Col>
+              </Row>
+                <Row>
+                  <Col md={{ span: 6, offset: 3 }}>
+                        <p>Phasellus bibendum arcu sit amet eros scelerisque pharetra.
+                            Phasellus sit amet ante non turpis congue sodales id sed odio.
+                            Pellentesque urna mauris, cursus in dapibus fringilla, rutrum sed metus.
+                        </p>
+                    </Col>
+                </Row>
+              <Row>
+                <Col className="cards-col">
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="//unsplash.it/200" />
+                        <Card.Body>
+                            <Card.Title>Card Title</Card.Title>
+                             <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col className="cards-col">
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="//unsplash.it/200" />
+                        <Card.Body>
+                            <Card.Title>Card Title</Card.Title>
+                             <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col className="cards-col">
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="//unsplash.it/200" />
+                        <Card.Body>
+                            <Card.Title>Card Title</Card.Title>
+                             <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+              </Row>
+            </Container>
     )
 }
 
--- src/components/HomeSection.js
+++ src/components/HomeSection.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import '../styles/HomeSection.sass';
-import Container from 'react-bootstrap/Container'
+
 
 const HomeSection = () => {
     return(
             <main>
-                <div id="space-above"></div>
+                <div id="space-a"></div>
                 <div id="container-home">
                     <h3>Lorem ipsum dolor amet</h3>
                     <p>Phasellus bibendum arcu sit amet</p>
