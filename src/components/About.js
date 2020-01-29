99f7c1392f3336fd11629f3ff6fdea6d2f3ceed8
TV321 <tvlahovic7@gmail.com>
2020-01-29 22:38:27 +0100

    animations, observer

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../styles/About.sass';
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
@@ -6,71 +6,131 @@ import Col from 'react-bootstrap/Col'
 import Card from 'react-bootstrap/Card'
 import CardImg from '../images/cardImg.jpg'
 import CarouselSection from './CarouselSection';
+import { motion } from "framer-motion"
 
 
-const About = () => {
-    return(
-            <Container id="about">
+class About extends Component{
+    state = {
+        intersecting: false
+    }
+    componentDidMount() {
+        const one = document.querySelector('#part-one')
+        const options = { }
+        const observer = new IntersectionObserver((entries, observer) =>{
+            console.log(entries[0].isIntersecting)
+            if(entries[0].isIntersecting) {
+                this.setState({
+                    intersecting: true
+                })
+            } else {
+                null
+            }
+        }, options)
+
+        observer.observe(one)
+    }
+
+
+    render() {
+
+        let one = ""
+        if (this.state.intersecting) {
+            one =
+            <React.Fragment>
+            <Row>
+              <Col>
+                  <motion.h2
+                      initial={{ y: "-100%", opacity: 0 }}
+                      animate={{ y: "0", opacity: 1 }}
+                      transition={{ duration: 1 }}
+                      >
+                      Etiam efficitur euismod
+                  </motion.h2>
+                  <motion.h3
+                      initial={{ y: "-100%", opacity: 0 }}
+                      animate={{ y: "0", opacity: 1 }}
+                      transition={{ duration: 1 }}
+                      >
+                      Praesent hendrerit lorem in augue
+                  </motion.h3>
+
+              </Col>
+            </Row>
+
               <Row>
-                <Col>
-                    <h2>Etiam efficitur euismod</h2>
-                    <h3>Praesent hendrerit lorem in augue</h3>
+                <Col md={{ span: 6, offset: 3 }}>
+                      <motion.p
+                          initial={{ y: "150%", opacity: 0 }}
+                          animate={{ y: "0", opacity: 1 }}
+                          transition={{ duration: 1 }}
 
-                </Col>
+                          >
+                          Phasellus bibendum arcu sit amet eros scelerisque pharetra.
+                          Phasellus sit amet ante non turpis congue sodales id sed odio.
+                          Pellentesque urna mauris, cursus in dapibus fringilla, rutrum sed metus.
+                      </motion.p>
+                  </Col>
               </Row>
+              </React.Fragment>
 
-                <Row>
-                  <Col md={{ span: 6, offset: 3 }}>
-                        <p>Phasellus bibendum arcu sit amet eros scelerisque pharetra.
-                            Phasellus sit amet ante non turpis congue sodales id sed odio.
-                            Pellentesque urna mauris, cursus in dapibus fringilla, rutrum sed metus.
-                        </p>
+        }
+
+
+
+        return(
+                <Container id="about">
+                    <div id="part-one">
+                        { one }
+                    </div>
+
+
+                    <CarouselSection />
+
+                  <Row>
+                    <Col className="cards-col" md={4}>
+                        <Card style={{ maxWidth: '360px' }}>
+                            <Card.Img variant="top" src={ CardImg } />
+                            <Card.Body>
+                                <Card.Title className="card-title">Euismod Cursus</Card.Title>
+                                 <Card.Subtitle className="mb-2 card-subtitle">Cras ac mattis dolor</Card.Subtitle>
+                                <Card.Text>
+                                    Some quick example text to build on the card title and make up the bulk of
+                                    the card's content.
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
                     </Col>
-                </Row>
-                <CarouselSection />
-              <Row>
-                <Col className="cards-col" md={4}>
-                    <Card style={{ maxWidth: '360px' }}>
-                        <Card.Img variant="top" src={ CardImg } />
-                        <Card.Body>
-                            <Card.Title className="card-title">Euismod Cursus</Card.Title>
-                             <Card.Subtitle className="mb-2 card-subtitle">Cras ac mattis dolor</Card.Subtitle>
-                            <Card.Text>
-                                Some quick example text to build on the card title and make up the bulk of
-                                the card's content.
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                </Col>
-                <Col className="cards-col" md={4}>
-                    <Card style={{ maxWidth: '360px' }}>
-                        <Card.Img variant="top" src={ CardImg } />
-                        <Card.Body>
-                            <Card.Title className="card-title">Euismod Cursus</Card.Title>
-                             <Card.Subtitle className="mb-2 card-subtitle">Cras ac mattis dolor</Card.Subtitle>
-                            <Card.Text>
-                                Some quick example text to build on the card title and make up the bulk of
-                                the card's content.
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                </Col>
-                <Col className="cards-col" md={4}>
-                    <Card style={{ maxWidth: '360px' }}>
-                        <Card.Img variant="top" src={ CardImg } />
-                        <Card.Body>
-                            <Card.Title className="card-title">Euismod Cursus</Card.Title>
-                             <Card.Subtitle className="mb-2 card-subtitle">Cras ac mattis dolor</Card.Subtitle>
-                            <Card.Text>
-                                Some quick example text to build on the card title and make up the bulk of
-                                the card's content.
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                </Col>
-              </Row>
-            </Container>
-    )
+                    <Col className="cards-col" md={4}>
+                        <Card style={{ maxWidth: '360px' }}>
+                            <Card.Img variant="top" src={ CardImg } />
+                            <Card.Body>
+                                <Card.Title className="card-title">Euismod Cursus</Card.Title>
+                                 <Card.Subtitle className="mb-2 card-subtitle">Cras ac mattis dolor</Card.Subtitle>
+                                <Card.Text>
+                                    Some quick example text to build on the card title and make up the bulk of
+                                    the card's content.
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col className="cards-col" md={4}>
+                        <Card style={{ maxWidth: '360px' }}>
+                            <Card.Img variant="top" src={ CardImg } />
+                            <Card.Body>
+                                <Card.Title className="card-title">Euismod Cursus</Card.Title>
+                                 <Card.Subtitle className="mb-2 card-subtitle">Cras ac mattis dolor</Card.Subtitle>
+                                <Card.Text>
+                                    Some quick example text to build on the card title and make up the bulk of
+                                    the card's content.
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                  </Row>
+                </Container>
+        )
+    }
+
 }
 
 
--- src/components/HomeSection.js
+++ src/components/HomeSection.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import '../styles/HomeSection.sass';
+import { motion } from "framer-motion"
 
+
+const variants = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1 },
+}
 
 const HomeSection = () => {
     return(
             <main id="home">
                 <div id="space-above"></div>
                 <div id="container-home">
-                    <h3>Lorem ipsum dolor amet</h3>
-                    <p>Phasellus bibendum arcu sit amet</p>
+                    <motion.h3
+                        initial={{ y: "-100%", opacity: 0 }}
+                        animate={{ y: "0", opacity: 1 }}
+                        transition={{ duration: 1 }}
+                        >
+                            Lorem ipsum dolor amet
+                    </motion.h3>
+                    <motion.p
+                        initial={{ y: "-100%", opacity: 0 }}
+                        animate={{ y: "0", opacity: 1 }}
+                        transition={{ duration: 1 }}
+                        >
+                        Phasellus bibendum arcu sit amet
+                    </motion.p>
                 </div>
             </main>
     )
